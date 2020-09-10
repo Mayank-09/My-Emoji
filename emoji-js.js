6e394a44b654e71d77a6427a62dfd731ede64968
Mayank-09 <70089444+Mayank-09@users.noreply.github.com>
2020-09-10 10:16:44 +0530

    Add files via upload

---
+++ emoji-js.js
@@ -0,0 +1,81 @@
+ const $grinningface= document.getElementById("grinningface");
+ const $facewithtearsofjoy= document.getElementById("facewithtearsofjoy");
+ const $grinningfacewithsmilingeyes=document.getElementById("grinningfacewithsmilingeyes");
+ const $winkingface=document.getElementById("smilingfacewithopenmouth");
+ const $smilingfacewithsunglasses=document.getElementById("smilingfacewithsunglasses");
+ const $confoundedface=document.getElementById("confoundedface");
+ const $loudlycryingface=document.getElementById("loudlycryingface");
+
+ $grinningface.onclick = () =>{
+     document.body.style.background ="url('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/241/grinning-face_1f600.png')";
+     document.body.style.backgroundSize = "100% 100%";
+$grinningface.style.backgroundColor = "#2f80ed";
+$facewithtearsofjoy.style.backgroundColor = "transparent";
+$grinningfacewithsmilingeyes.style.backgroundColor = "transparent";
+$winkingface.style.backgroundColor = "transparent";
+$smilingfacewithsunglasses.style.backgroundColor = "transparent";
+$confoundedface.style.backgroundColor = "transparent";
+$loudlycryingface.style.backgroundColor = "transparent";
+}
+
+$loudlycryingface.onclick = () =>{
+    document.body.style.background ="url('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/241/loudly-crying-face_1f62d.png')";
+    document.body.style.backgroundSize = "100% 100%";
+$loudlycryingface.style.backgroundColor = "#0000ff";
+$grinningface.style.backgroundColor = "transparent";
+$grinningfacewithsmilingeyes.style.backgroundColor = "transparent";
+$winkingface.style.backgroundColor = "transparent";
+$smilingfacewithsunglasses.style.backgroundColor = "transparent";
+$confoundedface.style.backgroundColor = "transparent";
+$facewithtearsofjoy.style.backgroundColor = "transparent";
+}
+
+$smilingfacewithsunglasses.onclick = () =>{
+    document.body.style.background ="url('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/241/smiling-face-with-sunglasses_1f60e.png')";
+    document.body.style.backgroundSize = "100% 100%";
+$smilingfacewithsunglasses.style.backgroundColor = "#ff0000";
+$grinningface.style.backgroundColor = "transparent";
+$grinningfacewithsmilingeyes.style.backgroundColor = "transparent";
+$winkingface.style.backgroundColor = "transparent";
+$loudlycryingface.style.backgroundColor = "transparent";
+$confoundedface.style.backgroundColor = "transparent";
+$facewithtearsofjoy.style.backgroundColor = "transparent";
+}
+
+$facewithtearsofjoy.onclick = () =>{
+    document.body.style.background ="url('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/241/face-with-tears-of-joy_1f602.png')";
+    document.body.style.backgroundSize = "100% 100%";
+$facewithtearsofjoy.style.backgroundColor = "#0000ff";
+$grinningface.style.backgroundColor = "transparent";
+$grinningfacewithsmilingeyes.style.backgroundColor = "transparent";
+$winkingface.style.backgroundColor = "transparent";
+$loudlycryingface.style.backgroundColor = "transparent";
+$confoundedface.style.backgroundColor = "transparent";
+$smilingfacewithsunglasses.style.backgroundColor = "transparent";
+}
+
+$confoundedface.onclick = () =>{
+    document.body.style.background ="url('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/241/confounded-face_1f616.png')";
+    document.body.style.backgroundSize = "100% 100%";
+$confoundedface.style.backgroundColor = "#ff0000";
+$grinningface.style.backgroundColor = "transparent";
+$grinningfacewithsmilingeyes.style.backgroundColor = "transparent";
+$winkingface.style.backgroundColor = "transparent";
+$loudlycryingface.style.backgroundColor = "transparent";
+$facewithtearsofjoy.style.backgroundColor = "transparent";
+$smilingfacewithsunglasses.style.backgroundColor = "transparent";
+}
+
+$grinningfacewithsmilingeyes.onclick = () =>{
+    document.body.style.background ="url('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/241/grinning-face-with-smiling-eyes_1f604.png')";
+    document.body.style.backgroundSize = "100% 100%";
+$grinningfacewithsmilingeyes.style.backgroundColor = "#0000ff";
+$grinningface.style.backgroundColor = "transparent";
+$confoundedface.style.backgroundColor = "transparent";
+$winkingface.style.backgroundColor = "transparent";
+$loudlycryingface.style.backgroundColor = "transparent";
+$facewithtearsofjoy.style.backgroundColor = "transparent";
+$smilingfacewithsunglasses.style.backgroundColor = "transparent";
+}
+
+
